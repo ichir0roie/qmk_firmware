fa83ea7247280f9ab0feb90811a7200a4cf6d59d
ichir0roieHome <ichir0roie.com>
2022-03-12 20:47:03 +0900

    update mouse move

--- keyboards/test22/keymaps/default/keymap.c
+++ keyboards/test22/keymaps/default/keymap.c
@@ -4,6 +4,7 @@
 #include QMK_KEYBOARD_H
 
 #include "joystick.h"
+#include "analog.h"
 
 
 // Defines names for use in layer keycodes and the keymap
@@ -19,13 +20,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_G,KC_H,KC_I
     )
 };
+# define maxAxis 1025
 //joystick config
 joystick_config_t joystick_axes[JOYSTICK_AXES_COUNT] = {
-    [0] = JOYSTICK_AXIS_IN(F4, 1000, 500,0),
-    [1] = JOYSTICK_AXIS_IN(F5, 0, 500,1000),
+    [0] = JOYSTICK_AXIS_IN(F4, maxAxis, maxAxis/2,0),
+    [1] = JOYSTICK_AXIS_IN(F5, 0, maxAxis/2,maxAxis),
 };
 
 
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
     return true;
+}
+
+int sensorX =0;
+int sensorY =0;
+void matrix_scan_user(){
+
+
+    sensorX=analogReadPin(F4);
+    sensorY=analogReadPin(F5);
+
+    if(sensorX<maxAxis/2-30){
+        register_code(KC_MS_R);
+        unregister_code(KC_MS_L);
+    }else if(sensorX>maxAxis/2+30){
+        register_code(KC_MS_L);
+        unregister_code(KC_MS_R);
+    }else{
+        unregister_code(KC_MS_L);
+        unregister_code(KC_MS_R);
+    }
+    if(sensorY<maxAxis/2-30){
+        register_code(KC_MS_U);
+        unregister_code(KC_MS_D);
+    }else if(sensorY>maxAxis/2+30){
+        register_code(KC_MS_D);
+        unregister_code(KC_MS_U);
+    }else{
+        unregister_code(KC_MS_U);
+        unregister_code(KC_MS_D);
+    }
+
 }
