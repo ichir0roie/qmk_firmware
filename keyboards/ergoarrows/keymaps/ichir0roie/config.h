083e847e4346463f72a1edd5d626abc1719e183c
ichir0roieHome <ichir0roie.com>
2022-03-17 16:16:31 +0900

    add 7skb

--- keyboards/ergoarrows/keymaps/ichir0roie/config.h
+++ keyboards/ergoarrows/keymaps/ichir0roie/config.h
@@ -35,6 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef TAPPING_TERM
 #define TAPPING_TERM 300
 #define TAPPING_TOGGLE 3
+#define HOLD_ON_OTHER_KEY_PRESS
 // #define TAPPING_FORCE_HOLD
 
 // #define MK_COMBINED
