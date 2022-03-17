4b1fe17c383290526923277109f62837f7386a80
ichir0roieHome <ichir0roie.com>
2022-03-17 14:19:00 +0900

    adjust

--- keyboards/ergoarrows/keymaps/ichir0roie/config.h
+++ keyboards/ergoarrows/keymaps/ichir0roie/config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #undef TAPPING_TERM
-#define TAPPING_TERM 150
+#define TAPPING_TERM 300
 #define TAPPING_TOGGLE 3
 // #define TAPPING_FORCE_HOLD
 
