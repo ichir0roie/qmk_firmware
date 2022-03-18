2734bca2ff41762d02a5cf7837af577d5103837b
ichir0roieHome <ichir0roie.com>
2022-03-18 10:52:09 +0900

    add 7skb

---
+++ keyboards/7skb/keymaps/ichir0roie/keymap.c
@@ -0,0 +1,60 @@
+#include <keymap_jp.h>
+#include <sendstring_jis.h>
+
+
+enum custom_keycodes {
+        QMKBEST = SAFE_RANGE
+    };
+
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case QMKBEST:if (record->event.pressed) SEND_STRING("QMK is the best thing ever!");break;
+    }
+    return true;
+};
+
+const key_override_t to0 = ko_make_basic(MOD_MASK_SHIFT, JP_ASTR, JP_TILD );
+const key_override_t to1 = ko_make_basic(MOD_MASK_SHIFT, JP_PLUS, JP_DLR );
+const key_override_t to2 = ko_make_basic(MOD_MASK_SHIFT, JP_MINS, JP_PERC );
+const key_override_t to3 = ko_make_basic(MOD_MASK_SHIFT, JP_DQUO, JP_CIRC );
+
+const key_override_t to4 = ko_make_basic(MOD_MASK_SHIFT, JP_QUOT, JP_GRV );
+const key_override_t to5 = ko_make_basic(MOD_MASK_SHIFT, JP_LPRN, JP_RPRN );
+const key_override_t to6 = ko_make_basic(MOD_MASK_SHIFT, JP_LCBR, JP_RCBR );
+const key_override_t to7 = ko_make_basic(MOD_MASK_SHIFT, JP_LBRC, JP_RBRC );
+
+// const key_override_t to8 = ko_make_basic(MOD_MASK_SHIFT, JP_BSLS, JP_YEN );
+
+const key_override_t to9 = ko_make_basic(MOD_MASK_SHIFT, JP_EQL, JP_EXLM );
+const key_override_t to10 = ko_make_basic(MOD_MASK_SHIFT, JP_SCLN, JP_AMPR );
+const key_override_t to11 = ko_make_basic(MOD_MASK_SHIFT, JP_COLN, JP_QUES );
+const key_override_t to12 = ko_make_basic(MOD_MASK_SHIFT, JP_SLSH, JP_PIPE );
+// const key_override_t to13 = ko_make_basic(MOD_MASK_SHIFT, JP_BSLS, JP_UNDS );
+
+
+// const key_override_t to = ko_make_basic(MOD_MASK_SHIFT, JP_, JP_ );
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &to0,
+    &to1,
+    &to2,
+    &to3,
+    &to4,
+    &to5,
+    &to6,
+    &to7,
+    // &to8,
+    &to9,
+    &to10,
+    &to11,
+    &to12,
+    // &to13,
+    // &to,
+    NULL  // Null terminate the array of overrides!
+};
+
+#include <keymapAG.c>
+
--- keyboards/ergoarrows/keymaps/ichir0roie/memo.md
+++ keyboards/ergoarrows/keymaps/ichir0roie/memo.md
@@ -1,7 +1,5 @@
 # all one
 
-mv Downloads/ichir0roie.json qmk_firmware/keyboards/ergoarrows/keymaps/ichir0roie/ichir0roie.json;qmk json2c -o qmk_firmware/keyboards/ergoarrows/keymaps/ichir0roie/keymapAG.c qmk_firmware/keyboards/ergoarrows/keymaps/ichir0roie/ichir0roie.json;qmk flash -kb ergoarrows -km ichir0roie
-
-# flash
-
+<!-- mv Downloads/ichir0roie_7skb.json qmk_firmware/myJsons/ichir0roie_7skb.json;3 -->
+qmk json2c -o keyboards/ergoarrows/keymaps/ichir0roie/keymapAG.c myJsons/ichir0roie_ergoarrows.json;
 qmk flash -kb ergoarrows -km ichir0roie
