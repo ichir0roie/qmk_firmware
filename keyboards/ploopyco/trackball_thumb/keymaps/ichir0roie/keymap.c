292bd721834797973ed1bc0726c4d07f1e22d167
togawa.ichiro <togawa.ichiro@kyla.co.jp>
2026-04-09 09:58:24 +0900

    use default define

--- keyboards/ploopyco/trackball_thumb/keymaps/ichir0roie/keymap.c
+++ keyboards/ploopyco/trackball_thumb/keymaps/ichir0roie/keymap.c
@@ -18,63 +18,12 @@
 #include <stdint.h>
 #include QMK_KEYBOARD_H
 
-// External variables from ploopyco.c
-extern float scroll_accumulated_h;
-extern float scroll_accumulated_v;
 
-enum custom_keycodes {
-    CUSTOM_DRAG_SCROLL = SAFE_RANGE,
-};
-
-bool set_scrolling = false;
-
-// Function to handle key events and enable/disable drag scrolling
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case CUSTOM_DRAG_SCROLL:
-            // Toggle set_scrolling when DRAG_SCROLL key is pressed or released
-            set_scrolling = record->event.pressed;
-            break;
-        default:
-            break;
-    }
-    return true;
-}
-
-// Modify these values to adjust the scrolling speed
-#define SCROLL_DIVISOR_H 8.0
-#define SCROLL_DIVISOR_V 8.0
-
-// Function to handle mouse reports and perform drag scrolling
-report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    // Check if drag scrolling is active
-    if (set_scrolling) {
-        // Calculate and accumulate scroll values based on mouse movement and divisors
-        scroll_accumulated_h += (float)mouse_report.x / SCROLL_DIVISOR_H;
-        scroll_accumulated_v += (float)mouse_report.y / SCROLL_DIVISOR_V;
-
-        // Assign integer parts of accumulated scroll values to the mouse report
-        mouse_report.h = (int8_t)scroll_accumulated_h;
-        mouse_report.v = -(int8_t)scroll_accumulated_v;
-
-        // Update accumulated scroll values by subtracting the integer parts
-        scroll_accumulated_h -= (int8_t)scroll_accumulated_h;
-        scroll_accumulated_v -= (int8_t)scroll_accumulated_v;
-
-        // Clear the X and Y values of the mouse report
-        mouse_report.x = 0;
-        mouse_report.y = 0;
-    } else {
-        mouse_report.h = 0;
-        mouse_report.v = 0;
-    }
-    return mouse_report;
-}
 
 // Modify these values to adjust the scrolling speed
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        CUSTOM_DRAG_SCROLL, MS_BTN1, KC_NO, MS_BTN2, KC_RIGHT_CTRL,
+        DRAG_SCROLL, MS_BTN1, KC_NO, MS_BTN2, KC_RIGHT_CTRL,
         MO(1)
     ),
     [1] = LAYOUT(
